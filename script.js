95358ca961352588a97d8362da845385c262ef27
saurabh-patil-9787 <154727373+saurabh-patil-9787@users.noreply.github.com>
2025-03-05 13:07:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+let firstInput = prompt("First input")
+let secondInput = prompt("second input")
+let se = parseInt(secondInput)
+
+let result = ((firstInput*2) + se)
+
+alert("The final result is: " + result)
